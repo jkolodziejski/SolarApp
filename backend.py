385edbb58b44c4c72097c30bdaa33e843ca4cd98
jkolodziejski <92368358+jkolodziejski@users.noreply.github.com>
2024-01-23 17:40:47 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,6 +2,47 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
+
+def CashPurchaseSavings(aCashPurchaseSavings):
+    paybackYears = []
+    panelConfigIndex = []
+    for i in aCashPurchaseSavings:
+        if (i['panelConfigIndex'] != -1):
+            panelConfigIndex.append(i["panelConfigIndex"])
+            paybackYears.append(i['cashPurchaseSavings']['paybackYears'])
+    plt.xticks(rotation=90)
+    plt.plot(panelConfigIndex, paybackYears)
+    plt.xlabel('Amount of panels')
+    plt.ylabel('Payback Years')
+    plt.title('Number of years until payback occurs')
+    plt.show()
+
+def SolarPanelConfig(aPotencial):
+    panelsCount = []
+    yearlyEnergyDcKwh = []
+    for i in aPotencial:
+        panelsCount.append(i["panelsCount"])
+        yearlyEnergyDcKwh.append(i["yearlyEnergyDcKwh"])
+    plt.bar(panelsCount, yearlyEnergyDcKwh)
+    plt.xlabel('Number of panels')
+    plt.ylabel('DC kWh')
+    plt.title('How much sunlight energy this layout captures over the course of a year')
+    plt.show()
+
+def BillsOver(aCashPurchaseSavings):
+    monthlyBill = []
+    panelConfigIndex = []
+    for i in aCashPurchaseSavings:
+        if (i['panelConfigIndex'] != -1):
+            monthlyBill.append(i["monthlyBill"]["units"])
+            panelConfigIndex.append(i["panelConfigIndex"])
+    plt.xticks(rotation=90)
+    plt.scatter(monthlyBill, panelConfigIndex)
+    plt.xlabel('monthly electric bill $')
+    plt.ylabel('Solar layout')
+    plt.title('Optimum solar layout for this bill size')
+    plt.show()
+
 def getStatistics(inputLatitude, intputLongitude, inputQuality):
     urlSolarAPI = 'https://solar.googleapis.com/v1/'
 
@@ -22,55 +63,18 @@ def getStatistics(inputLatitude, intputLongitude, inputQuality):
 
     print(data["imageryDate"]["year"])
     print(data["imageryDate"]["month"])
-
-    def SolarPanelConfig(aPotencial):
-        panelsCount = []
-        yearlyEnergyDcKwh = []
-        for i in aPotencial:
-            panelsCount.append(i["panelsCount"])
-            yearlyEnergyDcKwh.append(i["yearlyEnergyDcKwh"])
-        plt.bar(panelsCount, yearlyEnergyDcKwh)
-        # Adding labels and title
-        plt.xlabel('Number of panels')
-        plt.ylabel('DC kWh')
-        plt.title('How much sunlight energy this layout captures over the course of a year')
-        plt.show()
-
     SolarPanelConfig(data["solarPotential"]["solarPanelConfigs"])
-
-    def BillsOver(aCashPurchaseSavings):
-        monthlyBill = []
-        panelConfigIndex = []
-        for i in aCashPurchaseSavings:
-            monthlyBill.append(i["monthlyBill"]["units"])
-            panelConfigIndex.append(i["panelConfigIndex"])
-        plt.xticks(rotation=90)
-        plt.scatter(monthlyBill, panelConfigIndex)
-        # Adding labels and title
-        plt.xlabel('monthly electric bill $')
-        plt.ylabel('Solar layout')
-        plt.title('Optimum solar layout for this bill size')
-        plt.show()
-
     BillsOver(data["solarPotential"]["financialAnalyses"])
+    CashPurchaseSavings(data["solarPotential"]["financialAnalyses"])
 
 
-    for i in data["solarPotential"]["financialAnalyses"]:
-
-            print()
 
 
-    def CashPurchaseSavings(aCashPurchaseSavings):
-        paybackYears = []
-        panelConfigIndex = []
-        for i in aCashPurchaseSavings:
-            if (i['panelConfigIndex'] != -1):
-                panelConfigIndex.append(i["panelConfigIndex"])
-                paybackYears.append(i['cashPurchaseSavings']['paybackYears'])
-        plt.xticks(rotation=90)
-        plt.plot(panelConfigIndex,paybackYears)
-        # Adding labels and title
-        plt.xlabel('Amount of panels')
-        plt.ylabel('Payback Years')
-        plt.title('Number of years until payback occurs')
-        plt.show()
+
+
+
+
+
+
+
+
